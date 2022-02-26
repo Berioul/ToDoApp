e3401259b1fcc067860c5fe568067f06b0487f35
André Schreck <moin@andreschreck.de>
2022-02-26 17:39:53 +0100

    some fixes

--- frontend/src/component/Todo.jsx
+++ frontend/src/component/Todo.jsx
@@ -1,27 +1,21 @@
 import React, {useState} from 'react';
-import TodoForm from "./TodoForm";
-import {RiCloseCircleLine} from "react-icons/ri";
-import {TiEdit} from 'react-icons/ti'
 
- function Todo({todos, completeTodo, removeTodo}) {
-    const [edit, SetEdit] = useState({
+export default function Todo({todos, completeTodo, removeTodo}) {
+    const [edit, setEdit] = useState({
         id: null,
         value: ''
     });
-    return todo.map((todo, index) => (
+    return todos.map((todo, index) => (
         <div
             className={todo.isComplte ? 'todo-row complete' : 'todo-row'}
             key={index}
         >
             <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-                {todo.text}
+                {todo.title}
             </div>
             <div className='icons'>
-                <RiCloseCircleLine onClick={() => removeTodo(todo.id)}
-                                   className='delete-icon'
-                />
-                <TiEdit onClick={() => setEdit({id: todo.id, value: todo.text})}
-                        className='edit-icon'/>
+                <button onClick={() => removeTodo(todo.id)} className='delete-icon'>Löschen</button>
+                <button onClick={() => setEdit({id: todo.id, value: todo.text})} className='edit-icon'>Edit</button>
             </div>
         </div>
 
